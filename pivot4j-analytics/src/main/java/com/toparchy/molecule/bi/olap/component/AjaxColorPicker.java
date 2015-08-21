b23dcf67d9975a1714f9e218b26d50479041ed78
王宇轩 <currying@gmail.com>
2015-08-21 16:07:31 +0800

    Signed-off-by: 王宇轩 <currying@gmail.com>

--- pivot4j-analytics/src/main/java/com/toparchy/molecule/bi/olap/component/AjaxColorPicker.java
+++ pivot4j-analytics/src/main/java/com/toparchy/molecule/bi/olap/component/AjaxColorPicker.java
@@ -7,7 +7,6 @@
  * ====================================================================
  */
 package com.toparchy.molecule.bi.olap.component;
-
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
